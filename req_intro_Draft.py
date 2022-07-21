3a907e033228e7a24c677f6c92d93bb28a3b534f
Savan Meggi <you@example.comsmeggi@spartaglobal.comgit config --global user.name Your>
2022-07-21 17:29:36 +0100

    Able to extract data from within the dict once changing it from byte to string

--- req_intro_Draft.py
+++ req_intro_Draft.py
@@ -3,3 +3,9 @@ import requests
 Sun_times = requests.get('https://api.sunrise-sunset.org/json?lan=-4.4203400%27')
 
 print(Sun_times.json())
+
+data = Sun_times.json()['results']
+
+print(data['sunrise'])
+print(data['sunset'])
+print(data['day_length'])
